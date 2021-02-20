834936bbc5063ee6fa8ce446b706cd22f27946ef
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-02-20 08:51:14 -0300

    Finished showing local weather and London weather, when cannot access user geolocation

--- script.js
+++ script.js
@@ -3,26 +3,44 @@ const icon = document.querySelector("img#icon");
 const place = document.querySelector("h1#place");
 const weatherDescription = document.querySelector("h3#description");
 const temperature = document.querySelector("h2#temperature");
+const couldNotAccess = document.querySelector("p#could-not-access");
+let coords = "q=London";
+let lat;
+let long;
 
 // Functions
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     (position) => {
-      let coords = `lat=${position.coords.latitude}&lon=${position.coords.longitude}`;
+      lat = position.coords.latitude;
+      long = position.coords.longitude;
+      const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=b201157d2845f79ad6e02f582e930d6f`;
+
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then((position) => {
+          temperature.textContent = `${Math.round(position.main.temp)} ºC`;
+          place.textContent = `${position.name}, ${position.sys.country}`;
+          icon.src = `http://openweathermap.org/img/wn/${position.weather[0].icon}@2x.png`;
+          weatherDescription.textContent = `${position.weather[0].description}`;
+        });
     },
     () => {
-      let coords = "q=London";
+      const api = `http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=b201157d2845f79ad6e02f582e930d6f`;
+
+      fetch(api)
+        .then((response) => {
+          return response.json();
+        })
+        .then((position) => {
+          temperature.textContent = `${Math.round(position.main.temp)} ºC`;
+          place.textContent = `${position.name}, ${position.sys.country}`;
+          icon.src = `http://openweathermap.org/img/wn/${position.weather[0].icon}@2x.png`;
+          weatherDescription.textContent = `${position.weather[0].description}`;
+          couldNotAccess.classList.remove("hide");
+        });
     }
   );
 }
-
-// Main function
-$.getJSON(
-  `http://api.openweathermap.org/data/2.5/weather?${coords}&units=metric&appid=b201157d2845f79ad6e02f582e930d6f`,
-  function (data) {
-    icon.src =
-      "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
-    weatherDescription.textContent = `${data.weather[0].description}`;
-    temperature.innerHTML = `${Math.round(data.main.temp)} ºC`;
-  }
-);
